8bb71cac713f24cce0982ee8965cbb096501f6e7
elmaaroufi1 <abdessamad.maaroufi12@gmail.com>
2023-01-19 16:00:53 +0100

    update portfolio

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -15,6 +15,7 @@ const Projects = () => {
       navigate('/branding')
     }}
   return (
+    <>
     <div className='projects' id='projects'>
       <h2><span>02.</span> Projects I've Done</h2>
       <div className="container">
@@ -61,6 +62,54 @@ const Projects = () => {
          </div>
       </div>
     </div>
+    {/* second section */}
+    <div className='projects' id='projects'>
+      <h2><span>02.</span> Projects I've Done</h2>
+      <div className="container">
+         <div className="card">
+          <img src='' alt="" />
+          <h3>Visual Identity</h3>
+          <p>logo, typography, and other design components that makes a 
+            brand stand out from competitors, and recognizable to consumers </p>
+          <div className="softwares">
+            <ul>
+              <li><Javascript className='icon' /></li>
+              <li><Html className='icon'/></li>
+            </ul>
+          </div>
+          <button onClick={onClick} id="branding" >Explore</button>
+         </div>
+
+         <div className="card">
+          <img src='' alt="" />
+          <h3>User Interfaces</h3>
+          <p>design process.some professional work in user research, ideation,
+             wireframing, prototyping, and usability testing!</p>
+          <div className="softwares">
+            <ul>
+            <li><ReactJS className='icon'/></li>
+            <li><Nodejs className='icon'/></li>
+            </ul>
+          </div>
+          <button onClick={onClick} id="ui" >Explore</button>
+         </div>
+
+         <div className="card">
+          <img src='' alt="" />
+          <h3>Digital Art</h3>
+          <p> part of the creative or presentation process,
+             presented  some coole effect in Anime and characters using digital technology!</p>
+          <div className="softwares">
+            <ul>
+            <li><Tailwindcss className='icon'/></li>
+            <li><Bootstrap className='icon'/></li>
+            </ul>
+          </div>
+          <button onClick={onClick} id="digital" >Explore</button>
+         </div>
+      </div>
+    </div>
+    </>
   )
 }
 
